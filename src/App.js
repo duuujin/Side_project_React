c3997c9b0045f92ed83faa72634fd5010bc2d0c5
DESKTOP-BTDJ0S7\Administrator <enwls5626@gmail.com>
2025-08-28 22:58:25 +0900

    commit-nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  let [title, changeTitle] = useState([
+    "남자 코트 추천",
+    "강남 우동맛집",
+    "파이썬독학",
+  ]);
+
+  let posts = "강남 고기 맛집";
+  let [like, plusLike] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <div className="list">
+        <h3>
+          {title[0]} <span onClick={() => {plusLike(like+1)}}>❤️</span> {like}{" "}
+        </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{title[1]}</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{title[2]}</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
     </div>
   );
 }
